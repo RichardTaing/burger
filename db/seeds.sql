637b5174195a07faf746520314c21c5dfe6d46df
Richard Taing <Richard.Taing@pexa.com.au>
2019-09-21 13:11:24 +1000

    Intial setup

---
+++ db/seeds.sql
@@ -0,0 +1,32 @@
+USE burgers_db;
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ("")
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ("")
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ("")
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ("")
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ("")
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ("")
+
